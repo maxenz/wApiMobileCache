fd11c2b06f3366d58e083ac6ee74a83d9587af83
maxenz <maximilianopoggio@gmail.com>
2014-09-02 16:11:05 -0300

    cambie el formatter para que devuelva json

--- wApiMobileForCache/App_Start/WebApiConfig.cs
+++ wApiMobileForCache/App_Start/WebApiConfig.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Web.Http;
 
 namespace wApiMobileForCache
@@ -9,6 +10,7 @@ namespace wApiMobileForCache
     {
         public static void Register(HttpConfiguration config)
         {
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
